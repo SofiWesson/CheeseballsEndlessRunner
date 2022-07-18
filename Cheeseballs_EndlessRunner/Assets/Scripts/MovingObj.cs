93e683ef9156f63fa41d640fccf637d599e6ed6b
Sofi Wesson <sofiwesson@gmail.com>
2022-07-18 16:07:35 +1000

    4:06pm objects move from right to left in background

---
+++ Cheeseballs_EndlessRunner/Assets/Scripts/MovingObj.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingObj : MonoBehaviour
+{
+    // public 
+    public float speed = 1;
+
+    // private
+    private Vector3 m_spawnPosition;
+    private Vector3 m_deSpawnPosition;
+
+    private float m_tValue = 0;
+
+    void Update()
+    {
+        if (m_tValue < 1)
+            m_tValue += Time.fixedDeltaTime * speed;
+        else if (m_tValue >= 1)
+            m_tValue = 0;
+
+        transform.position = Vector3.Lerp(m_spawnPosition, m_deSpawnPosition, m_tValue);
+    }
+
+    public void SetStartAndEndPosition(Vector3 a_spawnPosition, Vector3 a_deSpawnPosition)
+    {
+        m_spawnPosition = a_spawnPosition;
+        m_deSpawnPosition = a_deSpawnPosition;
+    }
+}
---
+++ Cheeseballs_EndlessRunner/Assets/Scripts/ObjHandler.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjHandler : MonoBehaviour
+{
+    // ================================== GENERAL ==================================
+    //public
+    public List<GameObject> objs = new List<GameObject>();
+
+    // private
+
+    // ================================== SPAWNING ==================================
+    // public
+    public Transform spawnLocation;
+    public Transform deSpawnLocation;
+    
+    // private
+    private float m_timer = 0;
+    [SerializeField] private float m_bottomSpawnTime = 1;
+    [SerializeField] private float m_topSpawnTime = 5;
+
+    private GameObject m_GOBeingSpawn;
+
+    // ================================== SCROLLING ==================================
+    // public
+    public float speed = 1;
+
+    private float m_tValue = 0;
+
+    // private
+    private List<Transform> m_BGObjsBackLog = new List<Transform>(); // backlog of objs to add to main list once its not being used
+    private List<Transform> m_backgroundObjects = new List<Transform>();
+    
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (m_timer <= 0)
+        {
+            if (objs.Count == 0)
+                m_GOBeingSpawn = objs[0]; // if only one thing use 1st index
+            else
+                m_GOBeingSpawn = objs[Random.Range(0, objs.Count - 1)]; // gets random object from list of possible objects
+
+            GameObject go = Instantiate(m_GOBeingSpawn);
+            go.transform.position = spawnLocation.position; // set position to right side of screen
+            MovingObj moving = go.GetComponent<MovingObj>();
+            moving.SetStartAndEndPosition(spawnLocation.position, deSpawnLocation.position);
+            m_timer = Random.Range(m_bottomSpawnTime, m_topSpawnTime); // sets new time until 
+        }
+        else if (m_timer > 0)
+            m_timer -= Time.fixedDeltaTime;
+    }
+}
