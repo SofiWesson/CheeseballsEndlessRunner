949003c4fa8fec289bef6dfce078ddf29c97a288
MayKovacs <92353243+MayKovacs@users.noreply.github.com>
2022-07-20 09:46:16 +1000

    Death Screen Implemented
    Death functionality still required, but death screen and function works

--- Cheeseballs_EndlessRunner/Assets/Scripts/GameManager.cs
+++ Cheeseballs_EndlessRunner/Assets/Scripts/GameManager.cs
@@ -5,14 +5,15 @@ using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-    public Text timerText;
+    public Text timerText, bestTimeText;
+    public GameObject titleScreen, jumpButton, slideButton, timerDisplay, deathScreen, bestTimeObject;
     public bool gameActive;
-    [SerializeField] private float gameTime;
+    [SerializeField] private float gameTime, bestTime;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        bestTime = 0;
     }
 
     // Update is called once per frame
@@ -22,17 +23,48 @@ public class GameManager : MonoBehaviour
         {
             UpdateTimer();
         }
+
+        /*if (Input.GetKeyDown("e"))
+        {
+            EndGame();
+        }*/
     }
 
     public void StartGame()
     {
         gameActive = true;
+
+        titleScreen.SetActive(false);
+        jumpButton.SetActive(true);
+        slideButton.SetActive(true);
+        timerDisplay.SetActive(true);
     }
 
     public void EndGame()
     {
         gameActive = false;
-        // Functionality here :P
+
+        jumpButton.SetActive(false);
+        slideButton.SetActive(false);
+        deathScreen.SetActive(true);
+    }
+
+    public void RestartGame()
+    {
+        if (gameTime > bestTime)
+        {
+            bestTime = gameTime;
+            HighestTimeUpdate();
+        }
+        bestTimeObject.SetActive(true);
+
+        gameActive = true;
+
+        jumpButton.SetActive(true);
+        slideButton.SetActive(true);
+        deathScreen.SetActive(false);
+
+        gameTime = 0;
     }
 
     private void UpdateTimer()
@@ -47,5 +79,17 @@ public class GameManager : MonoBehaviour
         int minutes = (int)gameTime / 60;
 
         timerText.text = "Time: " + minutes + ":" + seconds.ToString("00") + ":" + fraction.ToString("00");
+    }
+
+    private void HighestTimeUpdate()
+    {
+        // This calculation is split in two, because for some reason, doing them in one line creates miscalculations, but separately it's fine
+        int fraction = Mathf.FloorToInt(bestTime * 100);
+        fraction %= 100;
+
+        int seconds = (int)bestTime % 60;
+        int minutes = (int)bestTime / 60;
+
+        bestTimeText.text = "Best Time: " + minutes + ":" + seconds.ToString("00") + ":" + fraction.ToString("00");
     }
 }
